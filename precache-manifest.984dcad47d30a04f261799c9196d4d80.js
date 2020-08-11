b7497b598c17e94b128e9a23e5e45f209b67348f
Parth Chandratre <parthchandratre80@gmail.com>
2020-08-11 22:17:32 +0530

    Updates

---
+++ precache-manifest.984dcad47d30a04f261799c9196d4d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0041b70225149fe9a57ab39ec4e776f7",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "c3b06e76f8dc63d20eac",
+    "url": "/Expense-Tracker/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/Expense-Tracker/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b06e76f8dc63d20eac",
+    "url": "/Expense-Tracker/static/js/main.fea28c00.chunk.js"
+  },
+  {
+    "revision": "242ecf752266ee6ab889",
+    "url": "/Expense-Tracker/static/js/runtime-main.c08c79a1.js"
+  }
+]);
